40ec927c6889651aadc6ab442548ed60fdbb88a0
ly2xxx <ly2xxx@hotmail.com>
2025-01-24 19:34:32 +0000

    initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # browser-use_poc
 browser-use poc
+
+Create a virtual environment :
+```bash
+python -m venv .venv
+.\.venv\Scripts\activate
+```
+
+Install the required dependencies in the virtual environment :
+```bash
+pip install -r requirements.txt
+```
+
+Install playwright dependencies in the virtual environment :
+```bash
+playwright install
+```
+
+# Lots more to explore on -
+@software{browser_use2024,
+  author = {Müller, Magnus and Žunič, Gregor},
+  title = {Browser Use: Enable AI to control your browser},
+  year = {2024},
+  publisher = {GitHub},
+  url = {https://github.com/browser-use/browser-use}
+}
---
+++ ollama_poc.py
@@ -0,0 +1,40 @@
+import os
+
+# Optional: Disable telemetry
+# os.environ["ANONYMIZED_TELEMETRY"] = "false"
+
+# Optional: Set the OLLAMA host to a remote server
+# os.environ["OLLAMA_HOST"] = "http://127.0.0.1:11434"
+
+import asyncio
+from browser_use import Agent
+from langchain_ollama import ChatOllama
+# from langchain.chat_models import ChatOllama
+# from langchain_community.chat_models import ChatOllama
+
+
+
+async def run_search() -> str:
+    agent = Agent(
+        # task="Search for a 'browser use' post on the r/LocalLLaMA subreddit and open it.",
+        # llm=ChatOllama(
+        #     model="deepseek-r1:latest",#"qwen2.5:32b-instruct-q4_K_M",
+        #     num_ctx=32000,
+        # ),
+        # llm=ChatOllama(model=os.getenv('OLLAMA_MODEL'), base_url=os.getenv('OLLAMA_BASE_URL'), temperature=0),
+        # task='Go to ebay.co.uk, search for laptop, sort by best rating, and give me the price of the first result',
+        task="Find flights on https://www.kayak.co.uk/ from Zurich to Beijing from 25.12.2024 to 02.02.2025.",
+        llm=ChatOllama(model="llama3.2:latest", base_url="http://127.0.0.1:11434", temperature=0),
+    )
+
+    result = await agent.run()
+    return result
+
+
+async def main():
+    result = await run_search()
+    print("\n\n", result)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
